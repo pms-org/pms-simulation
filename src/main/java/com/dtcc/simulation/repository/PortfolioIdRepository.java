9e329fbdef09f1ee636cae8eba72887fe6a31496
Your Name <neharikakumar05@gmail.com>
2026-01-28 11:22:01 +0530

    fixed cache to list

--- src/main/java/com/dtcc/simulation/repository/PortfolioIdRepository.java
+++ src/main/java/com/dtcc/simulation/repository/PortfolioIdRepository.java
@@ -1,9 +1,16 @@
 package com.dtcc.simulation.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dtcc.simulation.entity.PortfolioId;
 
-public interface PortfolioIdRepository extends JpaRepository<PortfolioId, UUID> {}
+public interface PortfolioIdRepository extends JpaRepository<PortfolioId, UUID> {
+
+    @Query("SELECT p.portfolio_id FROM PortfolioId p")
+    List<UUID> findAllPortfolioIds();
+
+}
--- src/main/java/com/dtcc/simulation/repository/SymbolRepository.java
+++ src/main/java/com/dtcc/simulation/repository/SymbolRepository.java
@@ -1,7 +1,15 @@
 package com.dtcc.simulation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dtcc.simulation.entity.Symbol;
 
-public interface SymbolRepository extends JpaRepository<Symbol, String> {}
+public interface SymbolRepository extends JpaRepository<Symbol, String> {
+
+    @Query("SELECT s.symbol FROM Symbol s")
+    List<String> findAllSymbols();
+
+}
